2f1035fe7535544b0990f1a64eda10fecdf6c986
MaxP <poppicht@googlemail.com>
2018-07-30 17:59:49 +0200

    Viele Änderungen Please don't fuck this up

---
+++ Project/SDL2_Engine/SDL2_Engine/Bullet.cpp
@@ -0,0 +1,13 @@
+#include "Enemy.h"
+#include "Vector2.h"
+#include "Engine.h"
+#include "Bullet.h"
+#include "Physic.h"
+#include <iostream>
+
+void GBullet::Update(float _deltaTime)
+{
+	CMoveObject::Update(_deltaTime);
+
+		
+}
